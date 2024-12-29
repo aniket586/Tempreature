e411ad265343b42fd1cb87467798e5ced98218d7
Aniket gaikwad <79001870+aniket586@users.noreply.github.com>
2024-12-29 13:25:05 +0530

    Update TempConverter.js

--- TempConverter/TempConverter.js
+++ TempConverter/TempConverter.js
@@ -1,36 +1,34 @@
-document.getElementById("num").addEventListener('input', function () {
-    var inputTemp = parseFloat(this.value);
-    var fromUnit;
-    if (inputTemp >= -100 && inputTemp <= 100) {
-        fromUnit = "Celsius";
-    } else {
-        fromUnit = "Fahrenheit";
-    }
-    document.getElementById("degreeSelect").value = fromUnit;
-    var toUnitDropdown = document.getElementById("SelectDegree");
-    toUnitDropdown.value = fromUnit === "Celsius" ? "Fahrenheit" : "Celsius";
-});
+document.getElementById('degreeSelect').addEventListener('click' , function(){
 
-function ConvertTemprature() {
-    var inputTemp = parseFloat(document.getElementById("num").value);
     var fromUnit = document.getElementById("degreeSelect").value;
-    var toUnit = document.getElementById("SelectDegree").value;
+    var toUnitDropdown = document.getElementById("selectDegree");
 
-    var result;
+if (fromUnit === "Celsius") {
+    toUnitDropdown.value = "Fahrenheit";
+} else if (fromUnit === "Fahrenheit") {
+    toUnitDropdown.value = "Celsius";
+};  
+})
+  
+function convertTemprature() {
+    var inputTemperature = parseFloat(document.getElementById("num").value);
+    var fromUnit = document.getElementById("degreeSelect").value;
+    var toUnit = document.getElementById("selectDegree").value;
+
     if (fromUnit === "Celsius") {
         if (toUnit === "Fahrenheit") {
-            result = (inputTemp * 9 / 5) + 32;
+            result = (inputTemperature * 9 / 5) + 32;
         } else {
-            result = inputTemp;
+            result = inputTemperature; 
         }
-    } else if (fromUnit === "Fahrenheit") {
+    } else if (fromUnit === "Fahrenheit") { 
         if (toUnit === "Celsius") {
-            result = ((inputTemp - 32) * 5) / 9;
+            result = ((inputTemperature - 32) * 5) / 9;
         } else {
-            result = inputTemp;
+            result = inputTemperature; 
         }
     }
 
     var resultBox = document.querySelector(".view");
-    resultBox.innerHTML = `<h4>${result.toFixed(2)}<span style="vertical-align: super; font-size: 20px;">°${toUnit.charAt(0)}</span></h4>`;
+    resultBox.innerHTML = `<h4>${result.toFixed(2)}<span style="vertical-align: super; font-size: 20px;">&deg;${toUnit.charAt(0)}</span></h4>`;
 }
